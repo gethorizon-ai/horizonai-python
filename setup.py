4bdec8aa1debe6782705872e5bb2c48fd996325c
ltawfik <linatawfik9@gmail.com>
2023-05-03 13:40:51 -0400

    added download_url to setup.py file

--- setup.py
+++ setup.py
@@ -17,12 +17,14 @@ setup(
     license="MIT",
     description="Python package and command line interface to access the Horizon AI API",
     url="www.gethorizon.ai",
+    download_url="https://github.com/gethorizon-ai/horizonai-python/archive/refs/tags/v0.1.0-alpha.tar.gz",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: MIT",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
 )
